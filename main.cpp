1af9f1aebef0ccdd479d1cca73ce9d72e70241ee
Afa Cheng <afa@afa.moe>
2021-05-09 07:36:29 -0400

    mbedtls & probability

--- main.cpp
+++ main.cpp
@@ -49,8 +49,8 @@ int generate_key(const int thread_id, const std::uint64_t max_iteration, bool *t
     {
         rc = mbedtls_ecdsa_genkey(&ecctx, MBEDTLS_ECP_DP_SECP256K1, mbedtls_ctr_drbg_random, &ctr_drbg);
 
-        mbedtls_ecp_point_write_binary(&(ecctx.grp), &(ecctx.Q), MBEDTLS_ECP_PF_UNCOMPRESSED,
-                                       &public_key_binary_len, public_key_binary, sizeof(public_key_binary));
+        rc = mbedtls_ecp_point_write_binary(&(ecctx.grp), &(ecctx.Q), MBEDTLS_ECP_PF_UNCOMPRESSED,
+                                            &public_key_binary_len, public_key_binary, sizeof(public_key_binary));
 
         keccak3_256(hash, 32, public_key_binary + 1, public_key_binary_len - 1);
 
@@ -59,7 +59,9 @@ int generate_key(const int thread_id, const std::uint64_t max_iteration, bool *t
         const std::uint8_t *address = hash + 12;
 
         // if (*(std::uint32_t *)(address + 16) == 0x88888888 /* && *(std::uint32_t *)(hash + 28) == 0x36363636*/)
-        if (*(std::uint16_t *)(address + 18) == 0x8888  && *(std::uint16_t *)(hash + 0) == 0x6666)
+        // if (*(std::uint16_t *)(address + 18) == 0x8888  && *(std::uint16_t *)(hash + 0) == 0x6666)
+        if (*(std::uint16_t *)(address + 18) == 0x8888)
+        // if (*(std::uint8_t *)(address + 19) == 0x88)
         {
             satisfied = true;
         }
@@ -82,19 +84,41 @@ int generate_key(const int thread_id, const std::uint64_t max_iteration, bool *t
     return 1;
 }
 
+double compute_probability50_addresses_count(double difficulty)
+{
+    return std::floor(std::log(0.5) / std::log(1. - (1. / difficulty)));
+}
+
+double compute_probability(double difficulty, std::uint64_t attempts)
+{
+    double prob = 1 - std::pow(1. - (1. / difficulty), attempts);
+    return std::round(10000. * prob) / 100.;
+};
+
+double compute_difficulty(unsigned int length)
+{
+    return std::pow(16, length);
+};
+
 int main()
 {
-    int nthreads = 0;
-    int tid = 0;
     bool terminate = false;
 
+    const int pattern_length = 4;
+    const double difficulty = compute_difficulty(pattern_length);
+    const double prob50addrs = compute_probability50_addresses_count(difficulty);
+
     std::uint64_t max = 1'000'000'000;
-    nthreads = ::omp_get_num_threads();
+    int nthreads = ::omp_get_max_threads();
 
-    std::uint64_t batchsize = 200'000;
+    std::uint64_t batchsize = 1000;
     std::uint64_t batchcount = max / batchsize;
+    std::uint64_t finished_batches = 0;
 
-#pragma omp parallel for schedule(static) shared(terminate)
+    printf("Scheduling %d batches for %d threads, batch size %d\n", batchcount, nthreads, batchsize);
+    printf("50%% probability addresses: %.0f, difficulty: %.0f\n", prob50addrs, difficulty);
+
+#pragma omp parallel for schedule(dynamic, 1) shared(terminate)
     for (int i = 0; i < batchcount; ++i)
     {
         if (terminate)
@@ -102,24 +126,30 @@ int main()
             continue;
         }
 
+        int tid = ::omp_get_thread_num();
+
+#pragma omp critical
+        printf("Starting batch %d, thread id %d\n", i, tid);
+
         auto start = std::chrono::high_resolution_clock::now();
 
-        tid = ::omp_get_thread_num();
         int result = generate_key(tid, batchsize, &terminate);
+        if (result == 0)
+        {
+            terminate = true;
+        }
 
         auto end = std::chrono::high_resolution_clock::now();
         auto duration = end - start;
         auto duration_ms = std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
         double key_per_second = batchsize / (duration_ms / 1000.);
 
-        if (result == 0)
-        {
-            terminate = true;
-        }
-
+#pragma omp atomic update
+        ++finished_batches;
+        double prob = compute_probability(difficulty, finished_batches * batchsize);
 
 #pragma omp critical
-        printf("Batch %d finished. Avg %f keys per second\n", i, key_per_second);
+        printf("Batch %d finished. Avg %f keys per second. Prob %.2f%%\n", i, key_per_second, prob);
     }
 
     return 0;
