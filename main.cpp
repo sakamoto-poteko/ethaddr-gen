09a97568f248c8e462112abe5c46e130083e7a7b
Afa Cheng <afa@afa.moe>
2021-05-09 04:50:01 -0400

    finished

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <cstdio>
 #include <climits>
 #include <cmath>
+
 #include <iostream>
 #include <iomanip>
+#include <chrono>
+
+#include <omp.h>
 
 #include <mbedtls/ecdsa.h>
 #include <mbedtls/ctr_drbg.h>
@@ -12,47 +16,17 @@
 
 #include "keccak-tiny.h"
 
-static void dump_buf(const char *title, unsigned char *buf, size_t len)
+static void print_hex(unsigned char *buf, size_t len)
 {
-    size_t i;
-
-    printf("%s", title);
-    for (i = 0; i < len; i++)
-        printf("%c%c", "0123456789ABCDEF"[buf[i] / 16],
-               "0123456789ABCDEF"[buf[i] % 16]);
-    printf("\n");
+    for (size_t i = 0; i < len; i++)
+    {
+        printf("%c%c", "0123456789ABCDEF"[buf[i] / 16], "0123456789ABCDEF"[buf[i] % 16]);
+    }
 }
 
-static void dump_pubkey(const char *title, mbedtls_ecdsa_context *key)
+int generate_key(const int thread_id, const std::uint64_t max_iteration, bool *terminate)
 {
-    unsigned char buf[300];
-    size_t len;
-
-    if (mbedtls_ecp_point_write_binary(&key->grp, &key->Q, MBEDTLS_ECP_PF_UNCOMPRESSED, &len, buf, sizeof buf) != 0)
-    {
-        printf("internal error\n");
-        return;
-    }
-
-    dump_buf(title, buf, len);
-
-    char priv[256] = {0};
-    if (mbedtls_mpi_write_string(&key->d, 16, priv, 256, &len) != 0)
-    {
-        printf("internal error\n");
-        return;
-    }
-    printf("priv: %s\n", priv);
-
-    char hash[32] = {0};
-    keccak3_256((std::uint8_t *)hash, 32, buf + 1, len - 1);
-
-    dump_buf("addr: ", (unsigned char *)hash, 32);
-}
-
-int main()
-{
-    int ret;
+    int rc;
 
     mbedtls_ecdsa_context ecctx;
     mbedtls_entropy_context entropy;
@@ -61,10 +35,92 @@ int main()
     mbedtls_ctr_drbg_init(&ctr_drbg);
     mbedtls_ecdsa_init(&ecctx);
     mbedtls_entropy_init(&entropy);
-    mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy, nullptr, 0);
-    mbedtls_ecdsa_genkey(&ecctx, MBEDTLS_ECP_DP_SECP256K1, mbedtls_ctr_drbg_random, &ctr_drbg);
 
-    dump_pubkey("  + Public key: ", &ecctx);
+    rc = mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy, nullptr, 0);
+
+    std::uint64_t counter = 0;
+    bool satisfied = false;
+
+    __attribute__((aligned(64))) std::uint8_t hash[32];
+    __attribute__((aligned(64))) std::uint8_t public_key_binary[384];
+    size_t public_key_binary_len;
+
+    while (!satisfied && counter++ < max_iteration && !*terminate)
+    {
+        rc = mbedtls_ecdsa_genkey(&ecctx, MBEDTLS_ECP_DP_SECP256K1, mbedtls_ctr_drbg_random, &ctr_drbg);
+
+        mbedtls_ecp_point_write_binary(&(ecctx.grp), &(ecctx.Q), MBEDTLS_ECP_PF_UNCOMPRESSED,
+                                       &public_key_binary_len, public_key_binary, sizeof(public_key_binary));
+
+        keccak3_256(hash, 32, public_key_binary + 1, public_key_binary_len - 1);
+
+        // satisfied = true;
+
+        const std::uint8_t *address = hash + 12;
+
+        // if (*(std::uint32_t *)(address + 16) == 0x88888888 /* && *(std::uint32_t *)(hash + 28) == 0x36363636*/)
+        if (*(std::uint16_t *)(address + 18) == 0x8888  && *(std::uint16_t *)(hash + 0) == 0x6666)
+        {
+            satisfied = true;
+        }
+    }
+
+    if (satisfied)
+    {
+#pragma omp critical
+        {
+            char priv[256] = {0};
+            size_t len;
+            mbedtls_mpi_write_string(&(ecctx.d), 16, priv, 256, &len) != 0;
+            printf("[thread %d]:\nPrivate key: %s\nAddress: ", thread_id, priv);
+            print_hex(hash + 12, 20);
+            printf("\n");
+        }
+        return 0;
+    }
+
+    return 1;
+}
+
+int main()
+{
+    int nthreads = 0;
+    int tid = 0;
+    bool terminate = false;
+
+    std::uint64_t max = 1'000'000'000;
+    nthreads = ::omp_get_num_threads();
+
+    std::uint64_t batchsize = 200'000;
+    std::uint64_t batchcount = max / batchsize;
+
+#pragma omp parallel for schedule(static) shared(terminate)
+    for (int i = 0; i < batchcount; ++i)
+    {
+        if (terminate)
+        {
+            continue;
+        }
+
+        auto start = std::chrono::high_resolution_clock::now();
+
+        tid = ::omp_get_thread_num();
+        int result = generate_key(tid, batchsize, &terminate);
+
+        auto end = std::chrono::high_resolution_clock::now();
+        auto duration = end - start;
+        auto duration_ms = std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
+        double key_per_second = batchsize / (duration_ms / 1000.);
+
+        if (result == 0)
+        {
+            terminate = true;
+        }
+
+
+#pragma omp critical
+        printf("Batch %d finished. Avg %f keys per second\n", i, key_per_second);
+    }
 
     return 0;
 }
